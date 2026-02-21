4e4f7397713d863073673200b1460f63af0ae961
iamrita-ai <amritanoob2005@gmail.com>
2026-02-21 07:08:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BOT_NAME = os.getenv("BOT_NAME")
-OWNER_ID = int(os.getenv("OWNER_ID"))
-API_ID = int(os.getenv("API_ID"))
+OWNER_ID = int(os.getenv("OWNER_ID", 0))
+API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH")
 
-FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")   # e.g., "@myChannel"
-SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL"))    # numeric channel ID
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))          # numeric channel ID
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")
+SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL", 0))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
 
 MAX_FILE_SIZE = 100 * 1024 * 1024  # 100 MB
 RESULTS_PER_PAGE = 10
