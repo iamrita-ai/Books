47df25012a4bebd1569a1e30a20138f2c6a16e68
iamrita-ai <amritanoob2005@gmail.com>
2026-02-22 07:04:10 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,13 @@
 from .channel import channel_handler
+from .source_group import source_group_handler_obj
 from .commands import get_handlers as get_command_handlers
 from .messages import group_message_handler_obj
 from .callbacks import callback_handler
 
-__all__ = ['channel_handler', 'get_command_handlers', 'group_message_handler_obj', 'callback_handler']
+__all__ = [
+    'channel_handler',
+    'source_group_handler_obj',
+    'get_command_handlers',
+    'group_message_handler_obj',
+    'callback_handler'
+]
