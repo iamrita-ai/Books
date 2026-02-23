de58ddfa42cbd16b2814b5e0890ac2e8d0e24083
iamrita-ai <amritanoob2005@gmail.com>
2026-02-23 21:40:14 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,10 +2,12 @@ from .source_group import source_group_handler_obj
 from .commands import get_handlers as get_command_handlers
 from .messages import group_message_handler_obj
 from .callbacks import callback_handler
+from .inline import inline_handler
 
 __all__ = [
     'source_group_handler_obj',
     'get_command_handlers',
     'group_message_handler_obj',
-    'callback_handler'
+    'callback_handler',
+    'inline_handler'
 ]
